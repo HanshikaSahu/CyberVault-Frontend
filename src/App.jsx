d753805d8059d12a173b354597617b808aacd118
aayushsah6969 <aayushsah269@gmail.com>
2024-12-26 14:31:05 +0530

    Initial Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+     <div>Hello world</div>
+     <div  className="text-3xl font-bold underline">Cyber-Vault-frontend</div>
+    </>
+  )
+}
+
+export default App
