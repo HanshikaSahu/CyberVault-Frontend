78991804396b71350a525bee316967bc8beb7454
akshatshrivastava1502 <akshat.shrivastava2005@gmail.com>
2024-12-28 21:30:17 +0530

    About section added

---
+++ src/components/about.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./about.scss";
+
+function aboutPage() {
+  return (
+    <div className="about-container">
+
+
+      <main className="aboutPage">
+        <h1>Welcome to Cybervault</h1>
+        <p className="subtitle">Securing the future, One Byte at a time</p>
+        <div className="aboutBox">We are a dynamic community of innovators, learners, and cybersecurity enthusiasts at KIIT Deemed To Be University. United by a shared passion for technology and security, we strive to empower students with cutting-edge knowledge, hands-on experiences, and opportunities to grow.</div>
+      </main>
+
+
+    </div>
+  );
+}
+
+export default aboutPage;
